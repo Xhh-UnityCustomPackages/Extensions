9f132ab0aeaa9431d3305a7232ceb9f3120f45d7
xuhonghua <tkonexuhh@gmail.com>
2025-02-26 10:32:12 +0800

    update

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Game;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Reset();
+    }
+}
